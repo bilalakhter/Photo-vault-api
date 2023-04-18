1f1d72c04bf199484d87105fc8ab9c45a3000e92
Bilal Akhtar <goldwinwill@gmail.com>
2023-04-18 16:31:01 +0500

    fixed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,9 +1,35 @@
 import { db } from "../src/db.server";
 
+type Book = {
+    title: String;
+    author: String;
+}
 
-type Image = {
-    name: String;
-    tag: String;
-    imageUrl: String;
-};
+async function seed() {
+    await Promise.all(
+        getBooks().map((Book) => {
+            const { title, author } = Book;
+            return db.Book.create({
+                data: {
+                    title,
+                    author
+                },
+            })
+        })
+    )
+}
 
+seed();
+
+function getBooks(): Array<Book> {
+    return [
+       {
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald"
+        },
+        {
+            title: "The Catcher in the Rye",
+            author: "J.D. Salinger"
+        }
+    ]
+}
