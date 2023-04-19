861071a323d5ac8b5e4596d04d47b2eded10aee8
unknown <goldwinwill@gmail.com>
2023-04-19 15:07:09 +0500

    added all routes

--- src/image.router.ts
+++ src/image.router.ts
@@ -13,3 +13,50 @@ imageRouter.get('/', async (request: Request, response: Response) => {
     return response.status(500).json(error.message);
   }
 });
+
+// create file
+imageRouter.post('/', async (request: Request, response: Response) => {
+  try {
+    const { title, tag, image } = request.body;
+    const createdFile = await Imageservice.createFile({ title, tag, image });
+    return response.status(201).json(createdFile);
+  } catch (error: any) {
+    return response.status(500).json(error.message);
+  }
+});
+
+// update file
+imageRouter.put('/:id', async (request: Request, response: Response) => {
+  try {
+    const { id } = request.params;
+    const { title, tag, image } = request.body;
+    const updatedFile = await Imageservice.updateFile(Number(id), { title, tag, image });
+    return response.status(200).json(updatedFile);
+  } catch (error: any) {
+    return response.status(500).json(error.message);
+  }
+});
+
+// get specific file by ID
+imageRouter.get('/:id', async (request: Request, response: Response) => {
+  try {
+    const { id } = request.params;
+    const file = await Imageservice.getFileById(Number(id));
+    if (!file) {
+      return response.status(404).json({ message: 'File not found' });
+    }
+    return response.status(200).json(file);
+  } catch (error: any) {
+    return response.status(500).json(error.message);
+  }
+});
+
+imageRouter.delete('/:id', async (request: Request, response: Response) => {
+  try {
+    const id = parseInt(request.params.id, 10);
+    await Imageservice.deleteFileById(id);
+    return response.sendStatus(204);
+  } catch (error: any) {
+    return response.status(500).json(error.message);
+  }
+});
--- src/image.service.ts
+++ src/image.service.ts
@@ -24,3 +24,86 @@ export const listFiles = async (): Promise<FileRead[]> => {
   });
 };
 
+export const getFileById = async (id: number): Promise<FileRead> => {
+  const file = await db.file.findUnique({
+    where: {
+      id,
+    },
+    select: {
+      id: true,
+      title: true,
+      tag: true,
+      image: true,
+    },
+  });
+
+  if (!file) {
+    throw new Error("File not found");
+  }
+
+  return file;
+};
+
+export const createFile = async (file: FileWrite): Promise<FileRead> => {
+  const encodedImage = file.image.toString("base64");
+  const createdFile = await db.file.create({
+    data: {
+      title: file.title,
+      tag: file.tag,
+      image: encodedImage,
+    },
+    select: {
+      id: true,
+      title: true,
+      tag: true,
+      image: true,
+    },
+  });
+
+  return createdFile;
+};
+
+
+
+
+export const updateFile = async (id: number, data: FileWrite): Promise<FileRead | null> => {
+  const file = await db.file.update({
+    where: { id },
+    data: {
+      title: data.title,
+      tag: data.tag,
+      image: data.image.toString("base64"), // Convert Buffer to base64 string
+    },
+    select: {
+      id: true,
+      title: true,
+      tag: true,
+      image: true,
+    },
+  });
+
+  if (!file) {
+    return null;
+  }
+
+  return {
+    id: file.id,
+    title: file.title,
+    tag: file.tag,
+    image: file.image,
+  };
+};
+
+export const deleteFileById = async (id: number): Promise<boolean> => {
+  const file = await db.file.delete({
+    where: {
+      id,
+    },
+  });
+
+  if (file === null) {
+    throw new Error("File not found");
+  }
+
+  return true;
+};
